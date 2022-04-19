b5764453fe0a2c8b79e4af11e53a5954eeee83e1
kayta <kaytahackman@gmail.com>
2022-04-19 13:49:48 +1000

    music API and styling updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Paper, Typography } from "@mui/material";
 export function Footer() {
     return (
         <div>
-            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={4}>
+            <Paper sx={{ position: 'fixed', bottom: 10, justifyContent: 'center'}} elevation={4}>
             <Typography>Created by Kayta, 2022</Typography>
             </Paper>
         </div>
---
+++ src/components/ShowMusic.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+interface AlbumProps {
+    album: string;
+}
+
+export function ShowMusic(props: AlbumProps) {
+    const [album, setAlbum] = useState<any>(null);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
