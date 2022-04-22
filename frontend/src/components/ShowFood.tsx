15ae73d8e5fa7211d10cabc09da12bed3720518d
kayta <kaytahackman@gmail.com>
2022-04-23 07:10:36 +1000

    refactoring show food function

--- frontend/src/components/ShowFood.tsx
+++ frontend/src/components/ShowFood.tsx
@@ -11,14 +11,6 @@ interface FoodProps {
 export function ShowFood(props: FoodProps) {
     const [recipe, setRecipe] = useState<any>(null);
 
-    useEffect(() => {
-        axios
-            .get(`http://localhost:3000/api/spoonacular/recipes/random?limitLicense=true&tags=${props.diet},${props.dishType}&number=1`)
-            .then((response) => {
-                setRecipe(response.data);
-            });
-    }, [props.diet, props.dishType]);
-
     return (
         <div>
             {recipe ? (
