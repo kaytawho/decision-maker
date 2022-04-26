91be36fdf619c5577ada2ac5135c48de8eab1bd3
kayta <kaytahackman@gmail.com>
2022-04-27 07:13:23 +1000

    updating navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { CssBaseline } from '@mui/material';
+import { BrowserRouter, Routes, Route, Link, } from "react-router-dom";
+import { AppBar, Button, CssBaseline, Toolbar, Typography } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { Home } from './components/HomePage';
 import { DecideFood } from './components/DecideFoodPage';
 import { DecideMusic } from './components/DecideMusicPage';
 import theme from './components/Theme';
-import { Navigation } from './components/Navigation';
 import { DecideMovie } from './components/DecideMoviePage';
 
 
@@ -44,33 +43,38 @@ function DecideMoviePage() {
   );
 }
 
+function Navigation() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+            <Route path="/decide-food" element={<DecideFoodPage />}/>
+              <Route path="/decide-music" element={<DecideMusicPage />}/>
+                <Route path="/decide-movie" element={<DecideMoviePage />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <Navigation />
-        <CssBaseline>
-          <BrowserRouter>
-              {/* <nav>
-                  <ul>
-                      <li>
-                      <Link to="/">Home</Link>
-                      </li>
-                      <li>
-                      <Link to="/decide-food">About</Link>
-                      </li>
-                      <li>
-                      <Link to="/decide-music">Login</Link>
-                      </li>
-                  </ul>
-              </nav> */}
-              <Routes>
-                <Route path="/" element={<HomePage />}/>
-                <Route path="/decide-food" element={<DecideFoodPage />}/>
-                <Route path="/decide-music" element={<DecideMusicPage />}/>
-                <Route path="/decide-movie" element={<DecideMoviePage />}/>
-              </Routes>
-          </BrowserRouter>
+          <CssBaseline>
+            <AppBar position="static" color="default" sx={{ color: 'white'}}>
+              <Toolbar sx={{ flexWrap: 'wrap', backgroundColor: 'black', padding: 3}}>
+                  <Typography variant="h4" color="inherit" noWrap sx={{ flexGrow: 1 }}> DECISI🤔N MAKER
+                      <Typography variant="body2">Created by Kayta, 2022</Typography>
+                  </Typography>
+                  <Button variant="contained"sx={{ p: 1, m: 1 }} style={{ backgroundColor: '#536DFE' }}>Home</Button>
+                    <Button variant="contained" href="/decide-food" sx={{ p: 1, m: 1 }} style={{ backgroundColor: '#536DFE' }}>Eat</Button>
+                      <Button variant="contained" href="/decide-music" sx={{ p: 1, m: 1 }} style={{ backgroundColor: '#536DFE' }}>Listen </Button>
+                        <Button variant="contained" href="/decide-movie" sx={{ p: 1, m: 1 }} style={{ backgroundColor: '#536DFE' }}>Watch </Button>
+              </Toolbar>
+            </AppBar>
+          <Navigation />
         </CssBaseline>
       </ThemeProvider>
     </div>
