ef5071d646d90bf3a7871dd4428a74c6ad4e2fd3
kayta <kaytahackman@gmail.com>
2022-04-27 22:21:38 +1000

    updating homepage

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -7,16 +7,16 @@ export function Home() {
     return (
         <div>
             <Paper>
-                <Grid container padding={10} justifyContent="center">
+                <Grid container padding={5} justifyContent="center">
                     <Grid item sx={{ height: "50%", width: 450 }}>
                         <Typography variant="h1" textAlign="left">Decision fatigue?</Typography>
-                    </Grid>
-                    <Grid item sx={{ height: "50%", width: 400, alignItems: 'center'}} >
-                        <Typography variant="h3" textAlign="left">Let's do the heavy lifting for you.</Typography>
-                        <Typography variant="h6" textAlign="left" sx={{ mt: 2}}>This is where life's most important decisions are made.</Typography>
-                        <Button variant="outlined" href="/decide-food" sx={{ mt: 2, backgroundColor: '#009688', color: 'white' }}>Get started </Button>
-                    </Grid>
-                </Grid>
+                            <Typography variant="h4" textAlign="left">Let's do the heavy lifting for you.</Typography>
+                                    <Button variant="outlined" href="/decide-food" sx={{ mt: 2, backgroundColor: '#009688', color: 'white' }}>Get started </Button>
+                                        </Grid>
+                                            <Grid item sx={{ height: "50%", width: 400, alignItems: 'center'}} >
+                                                <img src={require("./static/people-decisions.png")} alt="illustration of four people" width="400" height="400" />
+                                            </Grid>
+                                        </Grid>
             </Paper>
     </div>
     );
